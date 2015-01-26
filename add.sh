3f84ebc29f1e66f98d3825a883ae0bbcd2ab741e
Kazune Takahashi <kazunetakahashi@gmail.com>
2015-01-27 01:48:21 +0900

    proof of thm 1.6.2

--- add.sh
+++ add.sh
@@ -1,1 +1,1 @@
-git add main.tex main.pdf intro.tex minimal_sol.tex second_sol.tex ref.tex ref.bib second_sol2.tex nonex.tex exist.tex extremal_sol.tex add.sh
+git add main.tex main.pdf intro.tex minimal_sol.tex second_sol.tex ref.tex ref.bib second_sol2.tex nonex.tex exist.tex extremal_sol.tex add.sh appendix.tex
